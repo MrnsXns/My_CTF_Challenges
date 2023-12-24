e4d92d50ef1ef5bf50de00bc107a49a67c064f15
Marinos Xynis <65809489+MrnsXns@users.noreply.github.com>
2023-12-24 22:48:19 +0200

    Update validate_flag.php

--- Jeopardy_CTF/main_/validate_flag.php
+++ Jeopardy_CTF/main_/validate_flag.php
@@ -9,13 +9,14 @@
     #$f= $_POST['buttonId'];
     #echo $f;
    
-    function updateDatabase($col){
+    function updateDatabase($col,$chal_points){
         // Checking, if post value is 
         // set by user or not 
         $DATABASE_HOST = 'localhost';
-        $DATABASE_USER = 'root';
-        $DATABASE_PASS = '';
-        $DATABASE_NAME = 'ctf_db';
+	$DATABASE_USER = 'ctf_user';
+	$DATABASE_PASS = 'ctf_user123';
+	$DATABASE_NAME = 'ctf_db';
+
         // Try and connect using the info above.
         $con = mysqli_connect($DATABASE_HOST, $DATABASE_USER, $DATABASE_PASS, $DATABASE_NAME);
         if (mysqli_connect_errno()) {
@@ -24,12 +25,12 @@
         }
         if ($stmt = $con->prepare('UPDATE users SET '.$col.' =? WHERE username=?')) {
             // We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
-            $value = 1 ;
+            $value = $chal_points;
 
             session_start();
             $user=$_SESSION['name'];
 
-            $stmt->bind_param('ss', $value,$user);
+            $stmt->bind_param('is', $value,$user);
             $stmt->execute();
             
             header('Location: http://'.$_SERVER['SERVER_ADDR'].'/Jeopardy_CTF/main_/congrats_page.php');
@@ -46,22 +47,22 @@
 
 
 	if (isset($_POST['REflag']) && hash('sha256',$_POST['REflag'])=="0ffdf6a2b35b74b4d46cad58daafba32f5239ee2d3cf2c176f3767ad076a1936"){
-     $a= "re flag ok";
-     echo json_encode($a);
-     updateDatabase("RE");
+     /*$a= "re flag ok";
+     echo json_encode($a);*/
+     updateDatabase("RE",35);
        
         
         
     }
     elseif (isset($_POST['WEBflag']) && hash('sha256',$_POST['WEBflag'])=="839f6652ffa510044c1e0d99a1f986656854dfab64da2a310593c45d8d932982"){
         echo " WEBflag ok";
-        updateDatabase("WEB");
+        updateDatabase("WEB",30);
         
     }
     elseif (isset($_POST['CRYPTOflag']) &&  hash('sha256',$_POST['CRYPTOflag'])=="e2ee80394d308103abcbc101c3979776b8244cab31016d004c6aa344be7f79f7"){
        #echo "CRYPTOflag ok";
        
-        updateDatabase("CRYPTO");
+        updateDatabase("CRYPTO",15);
        
         
         
@@ -70,7 +71,7 @@
     elseif (isset($_POST['STEGOflag']) &&  hash('sha256',$_POST['STEGOflag'])=="a214c05afb09e0bb7ec726e464f76892bb1e7f7313457a03d56fb37fe82d3db9"){
         #echo "CRYPTOflag ok";
         
-         updateDatabase("STEGO");
+         updateDatabase("STEGO",20);
         
          
          
@@ -84,6 +85,5 @@
     }
     
 ?> 
-
 
 
