9c12f7d6358884403a445ca2b428651e288bc249
Marinos Xynis <65809489+MrnsXns@users.noreply.github.com>
2023-12-24 22:43:51 +0200

    Update database.sql

--- Jeopardy_CTF/database.sql
+++ Jeopardy_CTF/database.sql
@@ -8,19 +8,19 @@ CREATE TABLE IF NOT EXISTS users (
     `username` VARCHAR(255) NOT NULL,
     `password` varchar(255) NOT NULL,
     `email` varchar(100) NOT NULL,
-    `RE` BOOLEAN NOT NULL,
-    `WEB` BOOLEAN NOT NULL,
-    `CRYPTO` BOOLEAN NOT NULL,
-    `STEGO` BOOLEAN NOT NULL,
-    `completed` FLOAT(3,2),
+    `RE` INT DEFAULT 0,
+    `WEB` INT DEFAULT 0,
+    `CRYPTO` INT DEFAULT 0,
+    `STEGO` INT DEFAULT 0,
+    `points` INT DEFAULT 0,
     `challenge_timestamp` TIMESTAMP DEFAULT NULL
 )engine=InnoDB;
 DELIMITER //
 CREATE TRIGGER calculate_completed
 BEFORE UPDATE ON users FOR EACH ROW
 BEGIN
-    SET NEW.completed = IFNULL(NEW.RE, 0) + IFNULL(NEW.WEB, 0) + IFNULL(NEW.CRYPTO, 0) + IFNULL(NEW.STEGO, 0);
-    SET NEW.completed = NEW.completed * 0.25 ;
+    SET NEW.points = NEW.RE + NEW.WEB + NEW.CRYPTO + NEW.STEGO;
+
 
     SET NEW.challenge_timestamp= CURRENT_TIMESTAMP;
 END;
