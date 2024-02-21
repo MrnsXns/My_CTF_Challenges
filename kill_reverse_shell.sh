0c6eb5aa0abf01adfe7762b23aa52f86b63ee89a
Marinos Xynis <65809489+MrnsXns@users.noreply.github.com>
2024-02-21 21:58:01 +0200

    Delete kill_reverse_shell.sh

--- kill_reverse_shell.sh
+++
@@ -1,4 +0,0 @@
-# detect and kills tcp reverse shell
-#based on specific payload in data 
-
-sudo tshark -l -Y 'tcp contains "<some kind of string>"' -T fields -e ip.src | while read -r line; do sudo tcpkill host $line;break; done
