7700faa33c8c6a52ba2b91daad92449af7d72db3
Marinos Xynis <65809489+MrnsXns@users.noreply.github.com>
2024-03-15 22:00:16 +0200

    Update iptables_rule.sh
    add extra iptables rules

--- iptables_rule.sh
+++ iptables_rule.sh
@@ -1,2 +1,15 @@
-#iptables rule that kills connection when attacker-user try to gain access to db_folder (kill reverse tcp shell connection)
-sudo iptables -A INPUT -m string --string "db_folder" -j DROP --algo kmp #db_folder contains the creds of database. 
+#1.(avoid mysql connection from remote shell or phpmyadmin)
+sudo iptables -A OUTPUT -m string --string "ctf_user123" -j DROP --algo kmp
+
+
+#2.(avoid getconnection from remote shell 
+sudo iptables -A OUTPUT -m string --string "dbcon.php" -j DROP --algo kmp
+
+#3.(avoid access to phpmyadmin interface)
+sudo iptables -A INPUT -m string --string "phpmyadmin" -j DROP --algo kmp
+
+#4.(in case of db hijack avoid queries execution)
+sudo iptables -A INPUT -m string --string "DELETE" -j DROP --algo kmp
+sudo iptables -A INPUT -m string --string "delete" -j DROP --algo kmp
+sudo iptables -A INPUT -m string --string "UPDATE" -j DROP --algo kmp
+sudo iptables -A INPUT -m string --string "update" -j DROP --algo kmp
