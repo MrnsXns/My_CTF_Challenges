99d2e859b9d1129b8cc3ee40498f57e5077cc508
Marinos Xynis <65809489+MrnsXns@users.noreply.github.com>
2025-08-20 18:32:20 +0300

    Update manage_user.php
    Add ini_set('session.cookie_httponly', 1)

--- Jeopardy_CTF/Admin/manage_user.php
+++ Jeopardy_CTF/Admin/manage_user.php
@@ -1,4 +1,7 @@
 <?php 
+
+ini_set('session.cookie_httponly', 1);
+
 session_start();
 // Connect to database.
 $DATABASE_HOST = 'localhost';
@@ -103,6 +106,7 @@ if (isset($_GET['userId']) && isset($_GET['usrnm']) && isset($_GET['email'])){
     <inpu.t type="text" id="lname" name="lname" value="Doe"><br><br>';
 }
     
+
 
 
    ?>
