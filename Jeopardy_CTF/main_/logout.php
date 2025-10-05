7c356edaf35d292631d3d9a0eddc9cfb1d43f9a7
Marinos Xynis <65809489+MrnsXns@users.noreply.github.com>
2025-10-05 19:51:45 +0300

    Update logout.php

--- Jeopardy_CTF/main_/logout.php
+++ Jeopardy_CTF/main_/logout.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
 
-$_SESSION["server_ip"]=$_SERVER['SERVER_ADDR'];
+
 
 session_destroy();
 
 // Redirect to the login page:
-header('Location: http://'.$_SERVER['SERVER_ADDR'].'/Jeopardy_CTF/login/login.php');
+header('Location:/Jeopardy_CTF/login/login.php');
 ?>
+
