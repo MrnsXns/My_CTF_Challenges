4cfe1f6abefb56e8abeed2b6a15d3f0da95ab6e6
Marinos Xynis <65809489+MrnsXns@users.noreply.github.com>
2025-10-14 21:14:37 +0300

    Update logout.php

--- Jeopardy_CTF/main_/logout.php
+++ Jeopardy_CTF/main_/logout.php
@@ -4,8 +4,11 @@ session_start();
 
 
 session_destroy();
-
+session_start();
+session_regenerate_id(true);
 // Redirect to the login page:
 header('Location:/Jeopardy_CTF/login/login.php');
+exit();
 ?>
+
 
